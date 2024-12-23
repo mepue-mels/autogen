58bd9c6a4b4f11f3296f6010ad3ddbda083ec04b
MJ Ronduen <rjhronduen@mymail.mapua.edu.ph>
2024-12-23 15:54:11 +0800

    added frames

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 from include import *
+import tkinter as tk
 
 url = decrypt_url()
 
-
 def main():
 	if connectivity_test(url):
-		print("yes")
-	else:
-		print("no")
+		root = tk.Tk()
+		root.geometry("1280x720")
 
+		label = tk.Label(root,
+				 text="Automatic Question Generation",
+				 font=("Helvetica", 64),
+				 )
+
+		label.pack(pady=150)
+
+		button = tk.Button(root, text="Start", font=("Helvetica", 24), width=10, height=2)
+		button.pack()
+
+		root.mainloop()
+	else:
+		print("Error: cloud not active")
 
 #entry point for window
 if __name__ == "__main__":
